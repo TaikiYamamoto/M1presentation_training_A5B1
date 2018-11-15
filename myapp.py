8e4bfb566be3a1a8bb91028ce7aba6405a9bb732
Taiki Yamamoto <yamat_0129@outlook.jp>
2018-11-15 14:58:49 +0900

    latest version
    For training script. Please coding the flask methods into this program.

--- myapp.py
+++ myapp.py
@@ -8,26 +8,15 @@ from ans_prediction import get_predicted_answer
 # Instantiate your own name as "app"
 app = Flask(__name__)
 
-# Processing when accessing p5draw
-@app.route('/p5draw')
-def p5draw():
-    return render_template('p5draw.html') # Render p5draw.html
+# Processing when accessing / (index)
 
-# Processing when accessing sketch.js
-@app.route('/sketch.js')
-def sketch():
-    return render_template('/sketch.js')  # Render sketch.js
+# Processing when accessing /p5draw
 
-# Processing when accessing demotext
-@app.route('/demotext', methods = ['POST'])
-def demotext():
-    img = request.get_json(force=True)
-    imgData = preprocess(img)
-    
-    print('got it!')
-    return(str(get_predicted_answer(imgData)))
+# Processing when accessing /sketch.js
+
+# Processing when accessing /demo_prediction with POST Method
 
 if __name__ == '__main__':
     # Run this API
-    # After running, please access 'localhost:5000/p5draw' with Web Browser.
+    # After running, please access 'localhost:5000/' with Web Browser.
     app.run(host = '0.0.0.0', port = 5000, threaded = True, debug = True)
